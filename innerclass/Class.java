20c4e106f4f77544efb1b9ffbe9db5434a83bd06
amansinghal01 <60426145+amansinghal01@users.noreply.github.com>
2020-01-30 15:05:10 +0530

    Add files via upload

---
+++ innerclass/Class.java
@@ -0,0 +1,26 @@
+class A
+{
+	public void q(){
+	class B{}
+	}
+
+	public void w(){
+	class B{}
+	}
+
+	public void e(){
+	class B{}
+	}
+
+	public void r(){
+	class B{}
+	}
+
+	public void t(){
+	class B{}
+	}
+
+	public void y(){
+	class B{}
+	}
+}
